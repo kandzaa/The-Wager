e2c673df350e3f73a00275e6cf3e0179a8ae14da
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-10-14 11:20:19 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command }) => ({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost',
-            protocol: 'ws'
-        },
-        cors: {
-            origin: [
-                'https://thewager.eu',
-                'http://localhost:8000',
-                'http://127.0.0.1:8000'
-            ],
-            credentials: true
-        }
-    },
+
     plugins: [
         tailwindcss(),
         laravel({
