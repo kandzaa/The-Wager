d31b49e1c784ddd9c0c13d734a4d8128873b247f
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-10-17 01:24:16 +0300

    ssss

--- database/migrations/2025_09_04_000000_create_wagers_table.php
+++ database/migrations/2025_09_04_000000_create_wagers_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -24,11 +23,6 @@ return new class extends Migration
             $table->timestamp('ended_at')->nullable();
             $table->unsignedBigInteger('winning_choice_id')->nullable();
             $table->timestamps();
-
-            $constraintExists = DB::select("SELECT COUNT(*) as count FROM information_schema.table_constraints WHERE table_name = 'wagers' AND constraint_name = 'wagers_winning_choice_id_foreign'");
-            if ($constraintExists[0]->count == 0) {
-                $table->foreign('winning_choice_id')->references('id')->on('wager_choices')->onDelete('set null');
-            }
         });
     }
 
