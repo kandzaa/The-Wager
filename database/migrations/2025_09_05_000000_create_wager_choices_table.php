1ce634f081f8d8b802d4fb1c914b5b8975a1e1c3
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-10-23 09:14:58 +0300

    again

--- database/migrations/2025_09_05_000000_create_wager_choices_table.php
+++ database/migrations/2025_09_05_000000_create_wager_choices_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 
 class CreateWagerChoicesTable extends Migration
@@ -16,17 +15,22 @@ class CreateWagerChoicesTable extends Migration
             $table->string('label')->nullable();
             $table->timestamps();
 
-            $table->foreign('wager_id')
-                ->references('id')
-                ->on('wagers')
-                ->onDelete('cascade');
+            // First create the column
+            $table->index('wager_id');
         });
 
+        // Then add the foreign key in a separate transaction
+        DB::transaction(function () {
+            Schema::table('wager_choices', function (Blueprint $table) {
+                $table->foreign('wager_id')
+                    ->references('id')
+                    ->on('wagers')
+                    ->onDelete('cascade');
+            });
+        });
     }
-
     public function down()
     {
         Schema::dropIfExists('wager_choices');
-        Log::info('Dropped wager_choices table');
     }
 }
