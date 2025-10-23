00315412d234771ef507c1c862eb6a817df354e5
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-10-23 12:49:56 +0300

    fix to user_friends

--- database/migrations/2025_09_07_192241_create_user_friends_table.php
+++ database/migrations/2025_09_07_192241_create_user_friends_table.php
@@ -1,20 +1,20 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public $withinTransaction = false;
     public function up()
     {
         Schema::create('user_friends', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('friend_id')->constrained('users')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('friend_id');
+            $table->string('status')->default('pending');
             $table->timestamps();
 
-            // Prevent duplicate friendships
             $table->unique(['user_id', 'friend_id']);
         });
     }
