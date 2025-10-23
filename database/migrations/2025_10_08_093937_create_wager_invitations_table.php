dd5b99fafb985fe03756ffa4ca13271ee64d850f
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-10-23 13:45:16 +0300

    invitations table fix

--- database/migrations/2025_10_08_093937_create_wager_invitations_table.php
+++ database/migrations/2025_10_08_093937_create_wager_invitations_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
     {
         Schema::create('wager_invitations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('wager_id')->constrained()->onDelete('cascade');
-            $table->foreignId('inviter_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('invitee_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('wager_id')->references('wager_id')->on('wagers')->onDelete('cascade');
+            $table->foreignId('inviter_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreignId('invitee_id')->nullable()->references('user_id')->on('users')->onDelete('cascade');
             $table->string('email');
             $table->string('token', 64)->unique();
             $table->string('status')->default('pending');
