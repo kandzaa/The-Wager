0f49668985d2a0223aa5e2da4f998b5cf431c5be
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2026-03-09 16:39:28 +0200

    auth in wagercontrolelr

--- app/Http/Controllers/WagerController.php
+++ app/Http/Controllers/WagerController.php
@@ -5,6 +5,7 @@ use App\Models\Wager;
 use App\Models\WagerBet;
 use App\Models\WagerInvitation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
