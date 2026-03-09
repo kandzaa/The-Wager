37631803ff279f73f0448220223425418540682e
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2026-03-09 14:48:49 +0200

    rule validation in wager controller

--- app/Http/Controllers/WagerController.php
+++ app/Http/Controllers/WagerController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class WagerController extends Controller
 {
