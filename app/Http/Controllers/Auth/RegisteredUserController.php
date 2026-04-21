47ec96ca7bea00595e31bf77386a52ac93a0ae06
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2026-04-21 12:55:03 +0300

    starting balance

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'balance' => 1000,
         ]);
 
         event(new Registered($user));
